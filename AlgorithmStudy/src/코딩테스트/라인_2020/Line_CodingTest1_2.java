4426dcf63351bb7e623eeffd8731c37e8c2bda82
Choiminho <chlals862@naver.com>
2020-04-07 21:01:45 +0900

    baekjoon study

--- AlgorithmStudy/src/코딩테스트/라인_2020/Line_CodingTest1_2.java
+++ AlgorithmStudy/src/코딩테스트/라인_2020/Line_CodingTest1_2.java
@@ -9,10 +9,8 @@ public class Line_CodingTest1_2 {
 
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-
-		Stack<Integer> stack = new Stack<Integer>();
-
 		int result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
 
 		for (int i = 0; i < input.length(); i++) {
 
@@ -22,22 +20,22 @@ public class Line_CodingTest1_2 {
 				result += 1;
 				System.out.println("input result = " + result);
 
-			} else if (input.charAt(i) == ')' || input.charAt(i) == '}' || input.charAt(i) == ']' || input.charAt(i) == '>') {
-				//stack.push(-1);
+			} else if (input.charAt(i) == ')' || input.charAt(i) == '}' || input.charAt(i) == ']'
+					|| input.charAt(i) == '>') {
 				result -= 1;
 				System.out.println("output result = " + result);
-			
-			
-			if(stack.isEmpty() || stack.peek() >= 0) {
-				result = -1;
-			} else if(result == 0) {
+				System.out.println("stack = " + stack);
+
+			} else
 				stack.pop();
-				result++;
-			}
-			
-			}
+			result++;
+
 		} // for
-			System.out.println("result = " + result);
+		if (result < 0) {
+			result = -1;
+
+		}
+		System.out.println("result = " + result);
 	}
 
 }
