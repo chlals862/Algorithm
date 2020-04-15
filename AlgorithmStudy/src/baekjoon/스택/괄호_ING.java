21cf59fa7564ad56d051867c0e0baf4c476ce4b9
Choiminho <chlals862@naver.com>
2020-04-16 03:01:23 +0900

    baekjoon

--- AlgorithmStudy/src/baekjoon/스택/괄호_ING.java
+++ AlgorithmStudy/src/baekjoon/스택/괄호_ING.java
@@ -8,65 +8,30 @@ import java.util.Stack;
 public class 괄호_ING {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-	
-	//static char[] ch = new char[100];
+
+	// static char[] ch = new char[100];
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		int inputNum = Integer.parseInt(br.readLine());
-	//	char ch;
 		
 		Stack<Character> stack = new Stack<Character>();
-		for(int i=0; i<inputNum; i++) {
+		for (int i = 0; i < inputNum; i++) {
 			String inputStr = br.readLine();
-			
-			for(int j=0;j<inputStr.length();j++) {
+
+			for (int j = 0; j < inputStr.length(); j++) {
 				char ch = inputStr.charAt(j);
-				
-				if(ch == '(' || stack.isEmpty()) {
-					stack.push(ch);
 
-				} else if(!stack.isEmpty() && ch == ')') {
+				// 여는 괄호일때
+				if (ch == '(') {
+					stack.push('(');
+				// 닫는 괄호일때
+				}else if(ch == ')') {
+					//여는 괄호를 pop함
+					if(!stack.isEmpty())
 						stack.pop();
-			}
-			
-		}
-			if(stack.isEmpty() ) {
-				System.out.println("YES");
-			}else
-				System.out.println("NO");
-			}
+				}
+				
+			}//j
+		}//i
 	}
-		
-			//ch = inputStr.charAt(inputStr.length());
-			//System.out.println(ch);
-			//char ch[] = inputStr.toCharArray();
-			//System.out.println("ch =  " + ch[i]);
-			
-			
-			
-			
-//			if (ch == '(' || stack.empty()) {
-//				stack.push(ch);
-//			} else if (ch == ')') {
-//				if (!stack.isEmpty()) {
-//					stack.pop();
-//				}
-//			}
-		//	System.out.println("stack = " + stack);
-		//	if(stack.isEmpty() || ch[i] == '(' ) {
-		//		stack.push(ch);
-		//	}
-			
-			
-		
-			
-		
-			//System.out.print("ch = " + ch);
-			
-			  //if(inputStr.charAt(i) == '(' || stack.isEmpty()) {
-				// stack.push();
-			 // }
-		
-
-	}
+}
--- AlgorithmStudy/src/baekjoon/정렬/국영수.java
+++ AlgorithmStudy/src/baekjoon/정렬/국영수.java
@@ -10,19 +10,20 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.StringTokenizer;
 
-public class 국영수_ING {
+public class 국영수 {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	static ArrayList<Student> student = new ArrayList<Student>();
 	static int inputNum;
+
 	static class Student {
 		String name;
 		int kor;
 		int eng;
 		int math;
-		
+
 		public Student(String name, int kor, int eng, int math) {
 			this.name = name;
 			this.kor = kor;
@@ -32,47 +33,46 @@ public class 국영수_ING {
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+
 		inputData();
 		sorting();
 		outputData();
-		
+
 		bw.flush();
 		bw.close();
 	}
 
 	private static void outputData() throws IOException {
-		for(Student s : student) 
-			bw.write(s.name+"\n");
-		
+		for (Student s : student)
+			bw.write(s.name + "\n");
+
 	}
 
 	private static void sorting() {
 		/*
-		 * 1. 국어 점수가 감소하는 순서
-		 * 2. 국어 점수가 같으면 영어 점수가 증가하는 순서
-		 * 3. 국어 점수와 영어 점수가 같으면 수학 점수가 감소하는 순서로
-		 * 4. 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로
-		 * (단, 아스키 코드에서 대문자는 소문자보다 작으므로 사전순으로 앞에 옴)
-		 * */
-			
+		 * 1. 국어 점수가 감소하는 순서 2. 국어 점수가 같으면 영어 점수가 증가하는 순서 3. 국어 점수와 영어 점수가 같으면 수학 점수가
+		 * 감소하는 순서로 4. 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로 (단, 아스키 코드에서 대문자는 소문자보다 작으므로 사전순으로
+		 * 앞에 옴)
+		 */
+
 		Collections.sort(student, new Comparator<Student>() {
 			@Override
 			public int compare(Student s1, Student s2) {
-					//return -Integer.compare(s1.guk, s2.guk);
-				//국어점수가 같으면 영어 점수가 증가하는 순서
-				if(s1.kor == s2.kor) { 
+				if (s1.kor == s2.kor) { 
+
+					if (s1.eng == s2.eng) { 
+
+						if (s1.math == s2.math) {
+							// 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서
+							return s1.name.compareTo(s2.name);
+						}
+						//국어 점수,영어 점수가 같을 때 수학점수가 감소하는 순서
+						return -Integer.compare(s1.math, s2.math);
+					}
+					//국어 점수가 같으면 영어 점수가 증가하는 순서
 					return Integer.compare(s1.eng, s2.eng);
-					//국어점수와 영어 점수가 같으면 수학점수가 감소하는 순서
-				} else if(s1.kor == s2.kor && s1.eng == s2.eng) {
-					//★★★★★ Integer앞에 -를 붙이며 내림차순으로 바뀜 꿀팁~
-					return -Integer.compare(s1.math, s2.math);
-					//모든 점수가 같으면 이름이 사전 순으로 증가하는 순서
-				} else if(s1.kor == s2.kor && s1.eng == s2.eng && s1.math == s2.math) {
-				//return Integer.(s1.name.compareTo(s2.name));
-					return 1;
-					//모든게 해당 하지 않는다면 국어 점수가 감소하는 순서
-				} else
+				} 
+					//국어 점수가 감소하는 순서
 					return -Integer.compare(s1.kor, s2.kor);
 			}
 		});
@@ -80,7 +80,7 @@ public class 국영수_ING {
 
 	private static void inputData() throws NumberFormatException, IOException {
 
-		 inputNum = Integer.parseInt(br.readLine());
+		inputNum = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < inputNum; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -88,7 +88,7 @@ public class 국영수_ING {
 			int kor = Integer.parseInt(st.nextToken());
 			int eng = Integer.parseInt(st.nextToken());
 			int math = Integer.parseInt(st.nextToken());
-			
+
 			student.add(new Student(name, kor, eng, math));
 		}
 	}
