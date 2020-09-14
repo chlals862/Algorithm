7b6e982a12df49bf17e88855c5f43c5dedb7c91e
Choiminho <chlals862@naver.com>
2020-09-14 22:49:41 +0900

    fail bfs

--- AlgorithmStudy/src/baekjoon/BFS/다리만들기_ing.java
+++ AlgorithmStudy/src/baekjoon/BFS/다리만들기_ing.java
@@ -1,99 +1,94 @@
 package baekjoon.BFS;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+5
+1 0 0 0 0
+0 0 0 0 0
+0 0 1 0 0
+0 0 0 0 1
+0 0 0 0 1 -> 2
+*/
 public class 다리만들기_ing {
-	static int N, num, time;
+	static int N, num, result, max;
+	static ArrayList<Integer> min;
 	static int[][] map;
 	static boolean[][] visit;
+	static Queue<int[]> q;
 	static int[] dr = { -1, 0, 1, 0 };
 	static int[] dc = { 0, 1, 0, -1 };
-	static Queue<int[]> q = new LinkedList<int[]>();
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 
 	public static void main(String[] args) throws IOException {
 		setData();
-	}
-
-	private static void setData() throws IOException {
-		st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-		visit = new boolean[N][N];
-		num = 1;
-		for (int row = 0; row < N; row++) {
-			st = new StringTokenizer(br.readLine());
-			for (int col = 0; col < N; col++) {
-				map[row][col] = Integer.parseInt(st.nextToken());
-			}
-		}
-		// printData();
 		logic();
 	}
 
 	private static void logic() {
+		q = new LinkedList<int[]>();
+		num = 1;
 		for (int row = 0; row < N; row++) {
 			for (int col = 0; col < N; col++) {
 				if (map[row][col] == 1 && !visit[row][col]) {
 					q.add(new int[] { row, col });
 					visit[row][col] = true;
 					map[row][col] = num;
-					bfs();
+					islandNum(num);
 					num++;
 				}
 			}
 		}
+		System.out.println("섬 정보");
 		printData();
+		min = new ArrayList<Integer>();
 		for (int row = 0; row < N; row++) {
 			for (int col = 0; col < N; col++) {
-				if (map[row][col] == 0 && !visit[row][col]) {
+				// 섬들에서 시작
+				if (map[row][col] != 0) {
+					int count = map[row][col]; // 현재 섬의 번호
 					q.add(new int[] { row, col });
-					visit[row][col] = true;
-					bfs2();
+					bfs(count);
+					min.add(result);
 				}
 			}
 		}
-		System.out.println(time);
+
 	}
 
-	private static void bfs2() {
-		System.out.println("??");
+	private static void bfs(int count) {
 		while (!q.isEmpty()) {
 			int size = q.size();
 			for (int i = 0; i < size; i++) {
 				int[] currentRC = q.poll();
 				int cr = currentRC[0];
 				int cc = currentRC[1];
-				if(map[cr][cc] == 2 || map[cr][cc] == 3) {
-					return;
-				}
 				for (int dir = 0; dir < 4; dir++) {
 					int nr = cr + dr[dir];
 					int nc = cc + dc[dir];
 					if (rangeCheck(nr, nc)) {
-						if(map[nr][nc] == 0 && !visit[nr][nc]) {
-							q.add(new int[] {nr,nc});
-							visit[nr][nc] = true;
-						}
+						if(map[nr][nc] != count && map[nr][nc] != 0) return; //자신섬이 아니고,0이 아니면
+						if(map[nr][nc] == count && visit[nr][nc]) continue; //자신섬이고,방문했으면 
+						q.add(new int[] {nr,nc});
+						visit[nr][nc] = true;
 					}
 				}
 			}
-			time++;
-			printData();
+			result++;
 		}
 	}
 
-	private static void bfs() {
+	private static void islandNum(int num) {
+
 		while (!q.isEmpty()) {
 			int size = q.size();
 			for (int i = 0; i < size; i++) {
@@ -104,7 +99,7 @@ public class 다리만들기_ing {
 					int nr = cr + dr[dir];
 					int nc = cc + dc[dir];
 					if (rangeCheck(nr, nc)) {
-						if (map[nr][nc] == 1 && !visit[nr][nc]) {
+						if (map[nr][nc] != 0 && !visit[nr][nc]) {
 							q.add(new int[] { nr, nc });
 							visit[nr][nc] = true;
 							map[nr][nc] = num;
@@ -113,7 +108,6 @@ public class 다리만들기_ing {
 				}
 			}
 		}
-
 	}
 
 	private static boolean rangeCheck(int nr, int nc) {
@@ -122,7 +116,30 @@ public class 다리만들기_ing {
 		return false;
 	}
 
+	private static void setData() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		visit = new boolean[N][N];
+		for (int row = 0; row < N; row++) {
+			st = new StringTokenizer(br.readLine());
+			for (int col = 0; col < N; col++) {
+				map[row][col] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+
 	private static void printData() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				System.out.print(map[row][col] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	private static void printData2() {
 		for (int row = 0; row < N; row++) {
 			for (int col = 0; col < N; col++) {
 				System.out.print(visit[row][col] + " ");
---
+++ AlgorithmStudy/src/baekjoon/BFS/바이러스.java
@@ -0,0 +1,59 @@
+package baekjoon.BFS;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 바이러스 {
+	static int allComputer, netComputer,result;
+	static int[][] network;
+	static boolean[] visit;
+	static Queue<Integer> q;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		setData();
+	}
+
+	private static void setData() throws IOException {
+		st = new StringTokenizer(br.readLine());
+
+		allComputer = Integer.parseInt(st.nextToken());
+		network = new int[allComputer + 1][allComputer + 1];
+		visit = new boolean[allComputer + 1];
+		st = new StringTokenizer(br.readLine());
+		netComputer = Integer.parseInt(st.nextToken());
+		for (int row = 0; row < netComputer; row++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			network[x][y] = network[y][x] = 1;
+		}
+		visit[1] = true;
+		bfs(1);
+		bw.write(result+"\n");
+		bw.flush();
+		bw.close();
+	}
+
+	private static void bfs(int x) {
+		q = new LinkedList<Integer>();
+		q.add(x);
+		while (!q.isEmpty()) {
+			Integer col = q.poll();
+			for (int row = 1; row < allComputer; row++) {
+				if (network[row][col] != 1 || network[col][row] != 1) continue;
+				if (visit[row]) continue;
+				q.add(row);
+				visit[row] = true;
+				result++;
+			}
+		}
+	}
+}
