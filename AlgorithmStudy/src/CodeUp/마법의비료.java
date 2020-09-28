4d9f27474fb487afae7e872ebeb77e9f8ff9eb5d
Choiminho <chlals862@naver.com>
2020-09-28 19:13:21 +0900

    bfs study

---
+++ AlgorithmStudy/src/CodeUp/마법의비료.java
@@ -0,0 +1,89 @@
+package CodeUp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 마법의비료 {
+	static int R,C,cnt;
+	static int[][] map;
+	static boolean[][] visit;
+	static Queue<int[]> q;
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1};
+	static ArrayList<Integer> list;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		setData();
+		logic();
+	}
+	private static void logic() throws IOException {
+		q = new LinkedList<int[]>();
+		list = new ArrayList<Integer>();
+		for(int row=0;row<R;row++) {
+			for(int col=0;col<C;col++) {
+				if(map[row][col] == 1 && !visit[row][col]) {
+					cnt = 1;
+					q.add(new int[] {row,col});
+					visit[row][col] = true;
+					bfs();
+					list.add(cnt);
+				}
+			}
+		}
+		double ans = 0;
+		for(double d : list) {
+			ans += Math.sqrt(d);
+		}
+		bw.write(String.format("%.4f", ans));
+		bw.flush();
+		bw.close();
+	}
+	private static void bfs() {
+		while(!q.isEmpty()) {
+			int size = q.size();
+			for(int i=0;i<size;i++) {
+				int[] currentRC = q.poll();
+				int cr = currentRC[0];
+				int cc = currentRC[1];
+				for(int dir=0;dir<4;dir++) {
+					int nr = cr + dr[dir];
+					int nc = cc + dc[dir];
+					if(rangeCheck(nr,nc)) {
+						if(map[nr][nc] == 1 && !visit[nr][nc]) {
+							q.add(new int[] {nr,nc});
+							visit[nr][nc] = true;
+							cnt++;
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	private static boolean rangeCheck(int nr, int nc) {
+		if(nr >= 0 && nr < R && nc >= 0 && nc < C) return true;
+			return false;
+	}
+	private static void setData() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		C = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		map = new int[R][C];
+		visit = new boolean[R][C];
+		for(int row=0;row<R;row++) {
+			st = new StringTokenizer(br.readLine());
+			for(int col=0;col<C;col++) {
+				map[row][col] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+}
