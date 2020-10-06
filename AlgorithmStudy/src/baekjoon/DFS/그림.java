eb8b0237999befaca3def64c2a694b89092a5a55
Choiminho <chlals862@naver.com>
2020-10-06 19:25:44 +0900

    dfs study

--- AlgorithmStudy/src/baekjoon/DFS/그림.java
+++ AlgorithmStudy/src/baekjoon/DFS/그림.java
@@ -8,19 +8,57 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class 현수막 {
-	static int R, C ,cnt;
+public class 그림 {
+	static int R,C,cnt,result;
+	static int max = 0;
 	static int[][] map;
 	static boolean[][] visit;
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1};
 	static ArrayList<Integer> list;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
-
 	public static void main(String[] args) throws IOException {
 		setData();
+		logic();
+		printData();
 	}
-
+	private static void printData() throws IOException {
+		bw.write(list.size()+"\n");
+		bw.write(max + "\n");
+		bw.flush();
+		bw.close();
+	}
+	private static void logic() {
+		for(int row=0;row<R;row++) {
+			for(int col=0;col<C;col++) {
+				if(map[row][col] == 1 && !visit[row][col]) {
+					result = 1;
+					dfs(row,col);
+					list.add(cnt);
+					max = (Math.max(max, result) == max ) ? max : result;
+				}
+			}
+		}
+	}
+	private static void dfs(int row, int col) {
+		visit[row][col] = true;
+		for(int dir=0;dir<4;dir++) {
+			int nr = row + dr[dir];
+			int nc = col + dc[dir];
+			if(rangeCheck(nr,nc)) {
+				if(map[nr][nc] == 1 && !visit[nr][nc]) {
+					dfs(nr,nc);
+					result++;
+				}
+			}
+		}	
+	}
+	private static boolean rangeCheck(int nr, int nc) {
+		if(nr >= 0 && nr < R && nc >= 0 && nc < C) return true;
+			return false;	
+	}
 	private static void setData() throws IOException {
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
@@ -28,43 +66,11 @@ public class 현수막 {
 		map = new int[R][C];
 		visit = new boolean[R][C];
 		list = new ArrayList<Integer>();
-		for (int row = 0; row < R; row++) {
+		for(int row=0;row<R;row++) {
 			st = new StringTokenizer(br.readLine());
-			for (int col = 0; col < C; col++) {
+			for(int col=0;col<C;col++) {
 				map[row][col] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int row=0;row<R;row++) {
-			for(int col=0;col<C;col++) {
-				if(map[row][col] == 1 && !visit[row][col]) {
-					dfs(row,col);
-					list.add(cnt);
-				}
-			}
-		}
-		bw.write(list.size()+"\n");
-		bw.flush();
-		bw.close();
 	}
-
-	private static void dfs(int row, int col) {
-		visit[row][col] = true;
-		int[] dr = {-1,0,1,0,-1,-1,1,1};
-		int[] dc = {0,1,0,-1,-1,1,1,-1};
-		for(int dir=0;dir<8;dir++) {
-			int nr = row + dr[dir];
-			int nc = col + dc[dir];
-			if(rangeCheck(nr,nc)) {
-				if(map[nr][nc] == 1 && !visit[nr][nc]) {
-					dfs(nr,nc);
-				}
-			}
-		}
-	}
-
-	private static boolean rangeCheck(int nr, int nc) {
-		if(nr >= 0 && nr < R && nc >= 0 && nc < C) return true;
-			return false;
-	}
-
 }
