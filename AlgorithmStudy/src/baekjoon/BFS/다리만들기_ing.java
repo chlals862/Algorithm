f598edbf8269a8709275c86e245c26435e3b4b89
Choiminho <chlals862@naver.com>
2020-10-26 22:52:54 +0900

    bfs study

--- AlgorithmStudy/src/baekjoon/BFS/다리만들기_ing.java
+++ AlgorithmStudy/src/baekjoon/BFS/다리만들기_ing.java
@@ -6,16 +6,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 //육지마다 numbering -> 해당 육지에서 다른 육지가 나온 값들 중에서 최소값 출력하기. 
 public class 다리만들기_ing {
-	static int N,num,count,min,result;
+	static int N,num;
 	static int max = Integer.MAX_VALUE;
-	static int[][] map;
-	static int[][] temp;
+	static int[][] inputMap;
+	static int[][] bridge;
 	static boolean[][] visit;
 	static Queue<int[]> q;
 	static ArrayList<Integer> list;
@@ -28,72 +27,37 @@ public class 다리만들기_ing {
 		setData();
 		logic();
 	}
-	private static void logic() {
+	private static void logic() throws IOException {
 		q = new LinkedList<int[]>();
-		
 		num = 1;
 		for(int row=0;row<N;row++) {
 			for(int col=0;col<N;col++) {
-				if(map[row][col] == 1 && !visit[row][col]) {
+				if(inputMap[row][col] == 1 && !visit[row][col]) {
 					list = new ArrayList<Integer>();
-					map[row][col] = num;
+					inputMap[row][col] = num;
 					q.add(new int[] {row,col});
 					visit[row][col] = true;
 					numbering(num);
 					num++;
-					//count++;
 				}
+			}
+		}
+		//----------------------------------------------------------
+		//각 섬마다 번호 부여 끝
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (inputMap[row][col] != 0) q.add(new int[] { row, col });
 			}
 		}
 		visit = new boolean[N][N];
-		for(int row=0;row<N;row++) {
-			for(int col=0;col<N;col++) {
-				if(map[row][col] > 0 && !visit[row][col]) {
-					q.add(new int[] {row,col});
-					visit[row][col] = true;
-					bfs();
-					result = Math.min(max, result);
-				}
-			}
+		bridge = new int[N][N];
+		BFS();
+		for (int result : list) {
+			if (max > result) max = result;
 		}
-		System.out.println(max);
-	}
-	private static void bfs() {
-		int count = 0;
-		boolean find = false;
-		
-		loop : while(!q.isEmpty()) {
-			int size = q.size();
-			for(int i=0;i<size;i++) {
-				int[] currentRC = q.poll();
-				int cr = currentRC[0];
-				int cc = currentRC[1];
-				int island = map[cr][cc];
-				for(int dir=0;dir<4;dir++) {
-					int nr = cr + dr[dir];
-					int nc = cc + dc[dir];
-					if(rangeCheck(nr,nc)) {
-						if(map[nr][nc] == 0 && !visit[nr][nc]) {
-							q.add(new int[] {nr,nc});
-							visit[nr][nc] = true;
-							temp[nr][nc] = 1;
-						}
-						if(map[nr][nc] > 0 && map[nr][nc] != island) {
-//							System.out.println("count = " + count);
-//							list.add(count);
-//							count++;
-//							result += 1;
-							find = true;
-							break loop;
-						}
-					}
-				}
-			}
-			count+=1; 
-			if(find) {
-				if(max > count) max = count;
-			}
-		}
+		bw.write(max + "\n");
+		bw.flush();
+		bw.close();
 	}
 	private static void numbering(int num) {
 		while(!q.isEmpty()) {
@@ -106,17 +70,41 @@ public class 다리만들기_ing {
 					int nr = cr + dr[dir];
 					int nc = cc + dc[dir];
 					if(rangeCheck(nr,nc)) {
-						if(map[nr][nc] == 1 && !visit[nr][nc]) {
+						if(inputMap[nr][nc] == 1 && !visit[nr][nc]) {
 							q.add(new int[] {nr,nc});
 							visit[nr][nc] = true;
-							map[nr][nc] = num;
+							inputMap[nr][nc] = num;
 						}
 					}
 				}
 			}
-			
 		}
 	}
+	//다리 놓기
+	private static void BFS() {
+		while(!q.isEmpty()) {
+			int size = q.size();
+			for(int i=0;i<size;i++) {
+				int[] currentRC = q.poll();
+				int cr = currentRC[0];
+				int cc = currentRC[1];
+				for(int dir=0;dir<4;dir++) {
+					int nr = cr + dr[dir];
+					int nc = cc + dc[dir];
+					if(rangeCheck(nr,nc)) {
+						if(inputMap[nr][nc] != 0 
+								&& inputMap[cr][cc] != inputMap[nr][nc])
+							list.add(bridge[cr][cc]+bridge[nr][nc]);
+						if(inputMap[nr][nc] == 0) {
+							q.add(new int[] {nr,nc});
+							inputMap[nr][nc] = inputMap[cr][cc];
+							bridge[nr][nc] = bridge[cr][cc]+1;
+							}
+						}
+					}
+				}
+			}
+		}
 	private static boolean rangeCheck(int nr, int nc) {
 		if(nr >= 0 && nr < N && nc >= 0 && nc < N) return true;
 			return false;
@@ -124,33 +112,13 @@ public class 다리만들기_ing {
 	private static void setData() throws IOException {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
+		inputMap = new int[N][N];
 		visit = new boolean[N][N];
-		temp = new int[N][N];
 		for(int row=0;row<N;row++) {
 			st = new StringTokenizer(br.readLine());
 			for(int col=0;col<N;col++) {
-				map[row][col] = Integer.parseInt(st.nextToken());
+				inputMap[row][col] = Integer.parseInt(st.nextToken());
 			}
 		}
-		//printMap();
-	}
-	private static void printMap() {
-		for(int row=0;row<N;row++) {
-			for(int col=0;col<N;col++) {
-				System.out.print(map[row][col] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-	private static void printMap2() {
-		for(int row=0;row<N;row++) {
-			for(int col=0;col<N;col++) {
-				System.out.print(temp[row][col] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
 	}
 }
--- AlgorithmStudy/src/baekjoon/BFS/무한부스터_ing.java
+++ AlgorithmStudy/src/baekjoon/BFS/무한부스터_ing.java
@@ -10,76 +10,99 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 무한부스터_ing {
-	static int R,C,result;
+	static int R, C,time;
 	static int[][] map;
 	static boolean[][] visit;
-	static Queue<int[]> q;
-	static int[] dr = {0,0,1,0};
-	static int[] dc = {0,1,0,0};
+	static Queue<int[]> q = new LinkedList<int[]>();
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
+
 	public static void main(String[] args) throws IOException {
 		setData();
 		logic();
 	}
-	private static void logic() throws IOException {
-		q.add(new int[] {0,0});
+
+	private static void logic() {
+		q.add(new int[] { 0, 0 });
 		visit[0][0] = true;
+		time = 1;
 		bfs();
-		bw.write(result+"\n");
-		bw.flush();
-		bw.close();
+		printVisit();
+		System.out.println(time);
 	}
+
 	private static void bfs() {
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			int size = q.size();
-			for(int i=0;i<size;i++) {
+			for (int i = 0; i < size; i++) {
 				int[] currentRC = q.poll();
 				int cr = currentRC[0];
 				int cc = currentRC[1];
 				int booster = map[cr][cc];
-				if(cr == R-1 && cc == C-1) {
-					System.out.println("goal");
-					return;
-				}
-				int nr = cr+booster;
-				int nc = cc+booster;
-				if(rangeCheck(nr,nc)) {
-					
+				System.out.println("Booster = " + booster);
+				for (int dir = 0; dir < 4; dir++) {
+					for (int k = 0; k < booster; k++) {
+						int nr = cr + dr[dir] + k;
+						int nc = cc + dc[dir] + k;
+						if (rangeCheck(nr, nc)) {
+							if(nr == R-1 && nc == C-1) {
+								return;
+							}
+							if(!visit[nr][nc]) {
+								q.add(new int[] {nr,nc});
+								visit[nr][nc] = true;
+							}
+						}
+					}
 				}
 			}
-			result++;
-			printData();
+			time++;
+			printVisit();
 		}
-		
 	}
-	private static void printData() {
+
+	private static void printVisit() {
 		for(int row=0;row<R;row++) {
 			for(int col=0;col<C;col++) {
-				System.out.print(visit[row][col] + " ");
+				System.out.print(visit[row][col]+"");
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
+
 	private static boolean rangeCheck(int nr, int nc) {
-		if(nr >= 0 && nr < R && nc >= 0 && nc < C) return true;
-			return false;
+		if (nr >= 0 && nr < R && nc >= 0 && nc < C)
+			return true;
+		return false;
 	}
+
 	private static void setData() throws IOException {
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		map = new int[R][C];
 		visit = new boolean[R][C];
-		q = new LinkedList<int[]>();
-		for(int row=0;row<R;row++) {
+		for (int row = 0; row < R; row++) {
 			st = new StringTokenizer(br.readLine());
-			for(int col=0;col<C;col++) {
+			for (int col = 0; col < C; col++) {
 				map[row][col] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		printMap();
 	}
+
+	private static void printMap() {
+		for (int row = 0; row < R; row++) {
+			for (int col = 0; col < C; col++) {
+				System.out.print(map[row][col] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
 }
