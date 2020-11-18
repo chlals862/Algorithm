4378f5b0196fdd610ac382c27982b8917144d6a7
Choiminho <chlals862@naver.com>
2020-11-18 21:31:28 +0900

    algorithm study

--- AlgorithmStudy/src/SW_EXPERT_ACADEMY/D4/파핑파핑지뢰찾기_ing.java
+++ AlgorithmStudy/src/SW_EXPERT_ACADEMY/D4/파핑파핑지뢰찾기_ing.java
@@ -5,120 +5,124 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 파핑파핑지뢰찾기_ing {
-	static int K, N,time;
-	static boolean flag;
+	static int K, N, result, startR, startC;
 	static char[][] input;
 	static int[][] map;
-	static int[][] visit;
+	static boolean[][] visit;
 	static int[] dr = { -1, 0, 1, 0, -1, -1, 1, 1 };
 	static int[] dc = { 0, 1, 0, -1, -1, 1, 1, -1 };
+	static boolean flag, bfsFlag;
 	static Queue<int[]> q;
-	static ArrayList<Integer> list;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+		setData();
+	}
+
+	/*
+	 * 0. while문 -> 전부 true일 때 (종료 조건) -> break걸기 1. numbering 2. 0인 곳부터 8방향 탐색 ->
+	 * 숫자 나오는데 까지만 bfs탐색 3. 0인곳이 전부 방문 했다면 , 방문하지 않았고 주변에 지뢰가 있는 칸 수 만큼 ++;
+	 */
+	private static void setData() throws IOException {
 		st = new StringTokenizer(br.readLine());
 		K = Integer.parseInt(st.nextToken());
 		for (int k = 1; k <= K; k++) {
-			
-			q = new LinkedList<int[]>();
-			list = new ArrayList<Integer>();
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			map = new int[N][N];
 			input = new char[N][N];
-			visit = new int[N][N];
+			map = new int[N][N];
+			visit = new boolean[N][N];
+			q = new LinkedList<int[]>();
 			for (int row = 0; row < N; row++) {
 				String str = br.readLine();
 				for (int col = 0; col < N; col++) {
 					input[row][col] = str.charAt(col);
-					if (input[row][col] == '.')
-						map[row][col] = 0;
-					else if (input[row][col] == '*') {
+					if (input[row][col] == '*') {
 						map[row][col] = -1;
-						visit[row][col] = 1;
+						visit[row][col] = true;
 					}
 				}
 			}
-			//System.out.println("초기");
-			//printMap();
-			//0클릭시 8방향 open, 지뢰가 아닌 숫자 클릭시 해당 칸만 open.
-			numbering();
-			//System.out.println("넘버링 후");
-			//printMap();
-			//지뢰만 방문된 상황
-			int count = 0;
-			int time = 0;
-			int num = 0;
-			for(int row=0;row<N;row++) {
-				for(int col=0;col<N;col++) {
-					if(map[row][col] == 0) {
-						count++;
-						if(count == 1) {
-							q.add(new int[] {row,col});
-							visit[row][col] = 1;
-							
-							bfs();
-							count = 0;
-							time++;
-							System.out.println("bfs 후");
-							printMap2();
-							System.out.println("time = " + time);
-							
-						}
-					}
+			logic(k);
+		}
+		bw.close();
+	}
+
+	private static void logic(int k) throws IOException {
+		numbering();
+		while (true) {
+			bfsFlag = false;
+			logic1();
+			printVisit();
+			System.out.println("result = " +result);
+		}
+
+	}
+
+	private static void logic1() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (map[row][col] == 0 && !visit[row][col]) {
+					bfsFlag = true;
+					startR = row;
+					startC = col;
 				}
 			}
+		}
+		if (bfsFlag) {
+			System.out.println("true");
+			q.add(new int[] { startR, startR });
+			visit[startR][startR] = true;
+			bfs();
+			result+=1;
 		}
 	}
 
 	private static void bfs() {
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			int size = q.size();
-			for(int i=0;i<size;i++) {
+			for (int i = 0; i < size; i++) {
 				int[] currentRC = q.poll();
 				int cr = currentRC[0];
 				int cc = currentRC[1];
-				for(int dir=0;dir<8;dir++) {
+				for (int dir = 0; dir < 8; dir++) {
 					int nr = cr + dr[dir];
 					int nc = cc + dc[dir];
-					if(rangeCheck(nr,nc)) {
-						if(map[nr][nc] == 0 && visit[nr][nc] == 0) {
-							q.add(new int[] {nr,nc});
-							visit[nr][nc] = 1;
-						}else if(map[nr][nc] > 0 && visit[nr][nc] == 0) visit[nr][nc] = 1;
-					}
-				}
-			}
-		}
-		
-	}
-
-	private static void numbering() {
-		for(int row=0;row<N;row++) {
-			for(int col=0;col<N;col++) {
-				if(map[row][col] == -1) {
-					for(int dir=0;dir<8;dir++) {
-						int nr = row+dr[dir];
-						int nc = col+dc[dir];
-						if(rangeCheck(nr,nc)) {
-							if(map[nr][nc] != -1) {
-								map[nr][nc] += 1;
-							}
+					if (rangeCheck(nr, nc)) {
+						if (map[nr][nc] == 0 && !visit[nr][nc]) {
+							q.add(new int[] { nr, nc });
+							visit[nr][nc] = true;
+						} else if (map[nr][nc] > 0 && !visit[nr][nc]) {
+							visit[nr][nc] = true;
 						}
 					}
 				}
 			}
 		}
-		
+	}
+
+	private static void numbering() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (!visit[row][col]) {
+					for (int dir = 0; dir < 8; dir++) {
+						int nr = row + dr[dir];
+						int nc = col + dc[dir];
+						if (rangeCheck(nr, nc)) {
+							if (map[nr][nc] == -1)
+								map[row][col] += 1;
+						}
+					}
+				}
+			}
+		}
 	}
 
 	private static boolean rangeCheck(int nr, int nc) {
@@ -127,19 +131,20 @@ public class 파핑파핑지뢰찾기_ing {
 		return false;
 	}
 
-	private static void printMap() {
+	private static void printVisit() {
 		for (int row = 0; row < N; row++) {
 			for (int col = 0; col < N; col++) {
-				System.out.print(map[row][col] + " ");
+				System.out.print(visit[row][col] + " ");
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-	private static void printMap2() {
+
+	private static void printMap() {
 		for (int row = 0; row < N; row++) {
 			for (int col = 0; col < N; col++) {
-				System.out.print(visit[row][col] + " ");
+				System.out.print(map[row][col] + " ");
 			}
 			System.out.println();
 		}
---
+++ AlgorithmStudy/src/baekjoon/구현/지뢰찾기.java
@@ -0,0 +1,168 @@
+package baekjoon.구현;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 지뢰찾기 {
+	static int N;
+	static String[][] result;
+	static int[][] map;
+	static char[][] input, output;
+	static boolean[][] visit, resultVisit;
+	static int[] dr = { -1, 0, 1, 0, -1, -1, 1, 1 };
+	static int[] dc = { 0, 1, 0, -1, -1, 1, 1, -1 };
+	static Queue<int[]> q = new LinkedList<int[]>();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static boolean mine;
+
+	public static void main(String[] args) throws IOException {
+		setData();
+		logic();
+	}
+
+	private static void logic() throws IOException {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (input[row][col] == '*') {
+					map[row][col] = -1;
+					visit[row][col] = true;
+				}
+			}
+		}
+
+		numbering();
+		change();
+		if(mine) {
+			check();
+		}
+		printMap();
+		bw.flush();
+		bw.close();
+	}
+
+	private static void check() {
+		for(int row=0;row<N;row++) {
+			for(int col=0;col<N;col++) {
+				if(map[row][col] == -1) result[row][col] = "*";
+			}
+		}
+	}
+
+	private static void change() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (resultVisit[row][col] && map[row][col] != -1) {
+					result[row][col] = Integer.toString(map[row][col]);
+				}else if(resultVisit[row][col] && map[row][col] == -1) {
+					mine = true;
+				}else result[row][col] = ".";
+			}
+		}
+	}
+
+	private static void numbering() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				if (map[row][col] == 0) {
+					q.add(new int[] { row, col });
+				}
+			}
+		}
+		bfs();
+
+	}
+
+	private static void bfs() {
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				int[] currentRC = q.poll();
+				int cr = currentRC[0];
+				int cc = currentRC[1];
+				for (int dir = 0; dir < 8; dir++) {
+					int nr = cr + dr[dir];
+					int nc = cc + dc[dir];
+					if (rangeCheck(nr, nc)) {
+						if (map[nr][nc] == -1) {
+							map[cr][cc] += 1;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	private static boolean rangeCheck(int nr, int nc) {
+		if (nr >= 0 && nr < N && nc >= 0 && nc < N)
+			return true;
+		return false;
+	}
+
+	private static void setData() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		result = new String[N][N];
+		map = new int[N][N];
+		input = new char[N][N];
+		output = new char[N][N];
+		visit = new boolean[N][N];
+		resultVisit = new boolean[N][N];
+		for (int row = 0; row < N; row++) {
+			String str = br.readLine();
+			for (int col = 0; col < N; col++) {
+				input[row][col] = str.charAt(col);
+			}
+		}
+		for (int row = 0; row < N; row++) {
+			String str = br.readLine();
+			for (int col = 0; col < N; col++) {
+				output[row][col] = str.charAt(col);
+				if (output[row][col] == 'x')
+					resultVisit[row][col] = true;
+			}
+		}
+
+		//printMap1();
+		//printMap2();
+	}
+
+	private static void printMap1() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				System.out.print(input[row][col] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	private static void printMap2() {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				System.out.print(output[row][col] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+
+	private static void printMap() throws IOException {
+		for (int row = 0; row < N; row++) {
+			for (int col = 0; col < N; col++) {
+				bw.write(result[row][col] + "");
+			}
+			bw.write("\n");
+		}
+
+	}
+}
