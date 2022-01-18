19f9da6bbe79b9cb5eefe43d38820b4f3af1f3b2
Choiminho <chlals862@naver.com>
2022-01-19 02:55:40 +0900

    study

--- AlgorithmStudy/src/baekjoon/구현/쉽게푸는문제_ing.java
+++ AlgorithmStudy/src/baekjoon/구현/쉽게푸는문제_ing.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class 쉽게푸는문제_ing {
 
 	//static int[] box;
-	static int num; 
-	static int a,b,sum; //입력값 a,b와 합계 sum
+	static int[] num; 
+	static int a,b,sum,count; //입력값 a,b와 합계 sum
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		//a = sc.nextInt();
-		//b = sc.nextInt();
+		a = sc.nextInt();
+		b = sc.nextInt();
+		count = 1;
 		sum = 0;
-		num = 1;
-		int[] box = new int[1001];
-		box[0] = 1;
-		//num = 1이면 1을 1번 넣고 1번 반복
-		//num = 2이면 num을 num만큼 num반복
-		if(box[1000] != 0) { // 배열의 끝까지 숫자를 다 채우기
-			//쉬운건데 왜 못해 시발.
+		num = new int[1000];
+		while(true) {
+			for(int i=0;i<=i;i++) {
+				num[i] = 1;
+			}
 			
+			if(num[1000] != 0) break;
 			
-	}
+		}
 		//sum += box[a]+box[b];
 		//System.out.println(sum);
 	}
---
+++ AlgorithmStudy/src/baekjoon/구현/홀수_ing.java
@@ -0,0 +1,65 @@
+package baekjoon.구현;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class 홀수_ing {
+	static int cnt,sum;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		//ArrayList<Integer> list = new ArrayList<Integer>();
+		
+//		for(int i=1;i<=7;i++) {
+//			list.add(sc.nextInt());
+//		}
+//		
+//		//System.out.println(list.get(1));
+//		for(int i=0;i<=list.size()-1;i++) {
+//			System.out.println(list.get(i));
+//			if(list.get(i).intValue() % 2 ==0) {
+//				System.out.println("난 짝수");
+//				list.remove(i).intValue();
+//			}
+//		}
+//		System.out.println(list);
+//		for(int i=0;i<list.size();i++) {
+//			sum += list.get(i);
+//		}
+//		System.out.println(sum);
+		//System.out.println(list);
+		
+		
+//		Iterator it = list.iterator();
+//		while(it.hasNext()) {
+//			int a = (Integer)it.next();
+//			if(a % 2 == 0) it.remove();
+//		}
+//		System.out.println(list);
+		//System.out.println(list);
+		//System.out.println(list.size());
+		//int cnt = 0;
+		//for(int i=0;i<=list.size()-1;i++) {
+		
+			//if(list.get(i).valueOf(i%2==0))) {
+			//	System.out.println(list.get(i)+"ss");
+				//list.remove(i);
+			}
+		
+//		if(list.size() == 7) {
+//			System.out.println(-1); //홀수가 없다면 -1
+//		}else {
+//			for(int i=0;i<=list.size()-1;i++) {
+//				sum += list.get(i);
+//			}
+//			Collections.sort(list);
+//			System.out.println("sum = " + sum);
+//		}
+		//System.out.println(list);
+	//}
+
+}
